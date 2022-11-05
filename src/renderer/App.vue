184fa8a4e51a063b64798ce10ab16f10fc9c2136
chenhui <gaischen@163.com>
2022-11-05 20:52:42 +0800

    for assets

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -12,4 +12,124 @@
       core-sync-mode-modal
       core-play-detail
 </template>
+<script>
+
+</script>
+<style lang="less">
+@import './assets/styles/index.less';
+@import './assets/styles/layout.less';
+
+html, body {
+  overflow: hidden;
+}
+
+body {
+  user-select: none;
+  height: 100vh;
+  box-sizing: border-box;
+}
+#root {
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  color: @color-theme_2-font;
+  background: @color-theme-bgimg @color-theme-bgposition no-repeat;
+  background-size: @color-theme-bgsize;
+  transition: background-color @transition-theme;
+  background-color: @color-theme;
+}
+
+.disableAnimation * {
+  transition: none !important;
+  animation: none !important;
+}
+
+.transparent {
+  padding: @shadow-app;
+  // #waiting-mask {
+  //   border-radius: @radius-border;
+  //   left: @shadow-app;
+  //   right: @shadow-app;
+  //   top: @shadow-app;
+  //   bottom: @shadow-app;
+  // }
+  #root {
+    box-shadow: 0 0 @shadow-app rgba(0, 0, 0, 0.5);
+    border-radius: @radius-border;
+    background-color: transparent;
+  }
+  #container {
+    border-radius: @radius-border;
+    background-color: transparent;
+  }
+}
+.disableTransparent {
+  background-color: #fff;
+
+  #right {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+
+  // #view { // 偏移5px距离解决非透明模式下右侧滚动条无法拖动的问题
+  //   margin-right: 5Px;
+  // }
+}
+.fullscreen {
+  background-color: #fff;
+
+  #right {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
+
+#container {
+  position: relative;
+  display: flex;
+  height: 100%;
+}
+
+#left {
+  flex: none;
+  width: @width-app-left;
+}
+#right {
+  flex: auto;
+  display: flex;
+  flex-flow: column nowrap;
+  transition: background-color @transition-theme;
+  background-color: @color-theme_2;
+
+  border-top-left-radius: @radius-border;
+  border-bottom-left-radius: @radius-border;
+  overflow: hidden;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.1);
+}
+#toolbar, #player {
+  flex: none;
+}
+#view {
+  position: relative;
+  flex: auto;
+  display: flex;
+  min-height: 0;
+}
+#view-container {
+  flex: auto;
+}
+
+each(@themes, {
+  #root.@{value} {
+    color: ~'@{color-@{value}-theme_2-font}';
+    background-color: ~'@{color-@{value}-theme}';
+    background-image: ~'@{color-@{value}-theme-bgimg}';
+    background-size: ~'@{color-@{value}-theme-bgsize}';
+    background-position: ~'@{color-@{value}-theme-bgposition}';
+    #right {
+      background-color: ~'@{color-@{value}-theme_2}';
+    }
+  }
+})
+</style>
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,3 +1,41 @@
 import '../common/error'
 import { createApp } from 'vue'
+import App from './App'
 
+import {rendererSend, rendererInvoke, NAMES } from '@common/ipc'
+
+
+rendererInvoke(NAMES.mainWindow.get_setting).then(( setting, version ) => {
+    global.appSetting = setting
+    // Set language automatically
+  if (!window.i18n.availableLocales.includes(setting.langId)) {
+    let langId = null
+    let locale = window.navigator.language.toLocaleLowerCase()
+    if (window.i18n.availableLocales.includes(locale)) {
+      langId = locale
+    } else {
+      for (const lang of langList) {
+        if (lang.alternate == locale) {
+          langId = lang.locale
+          break
+        }
+      }
+      if (langId == null) langId = 'en-us'
+    }
+    setting.langId = langId
+    rendererSend(NAMES.mainWindow.set_app_setting, setting)
+    console.log('Set lang', setting.langId)
+  }
+
+  createApp({
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }).mount('#root')
+  
+//   const app = createApp(App)
+  console.log("create vue app success....start mount #root")
+//   app.mount('#root')
+})
