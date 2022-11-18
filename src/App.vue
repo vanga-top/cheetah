a165c039391f29a7f9e01dd3979fa650564c9686
chenhui <gaischen@163.com>
2022-11-18 20:41:04 +0800

    app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <LeftNav />
+  <el-row :span="24">
+    top bar
+  </el-row>
+  <el-row>
+    <el-col :span="6"><div class="grid-content ep-bg-purple" />
+      <LeftNav />
+    </el-col>
+    <el-col :span="18"><div class="grid-content ep-bg-purple-light" />
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -23,6 +32,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
 </style>
